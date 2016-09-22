4005cd886eb09de2cd93786e161c39596b16b4df
Nick <saltblade@gmail.com>
2016-09-22 08:37:34 -0600

    udpate

--- WorkoutDWP/src/controllers/UserController.java
+++ WorkoutDWP/src/controllers/UserController.java
@@ -59,7 +59,7 @@ public class UserController {
 	public ModelAndView getLogin(String username, String password) {
 		Account account = workoutDao.userHasAccount(username, password);
 		MyDate date = new MyDate();
-System.out.println("INHERE");
+
 		List<WorkoutDefinition> wds = new ArrayList<>();
 		for (int i = 1; i <= date.getDays(); i++) {
 			if (workoutDao.getUserWorkout(date.getYear(), date.getMonthInt(), i, account.getId()) != null) {
